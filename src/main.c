5c192fa98ee9360e667c548c947cf5ac9b64d67f
HeavyRain266 <ignacy423@gmail.com>
2022-04-09 16:02:18 +0200

    Fix indents on GitHub

--- src/main.c
+++ src/main.c
@@ -8,9 +8,9 @@
 
 #define EXIT(msg) { fputs(msg, stderr); exit(EXIT_FAILURE); }
 
+static struct xkb_state  *xkb_state;
+static struct xkb_keymap *xkb_keymap;
 static struct xkb_context *xkb_context;
-static struct xkb_keymap *keymap;
-static struct xkb_state *xkb_state;
 
 static void
 process_event(struct libinput_event *event)
@@ -22,8 +22,8 @@ process_event(struct libinput_event *event)
         struct libinput_event_keyboard *keyboard_event = libinput_event_get_keyboard_event(event);
 
         uint8_t key = libinput_event_keyboard_get_key(keyboard_event);
-        int state = libinput_event_keyboard_get_key_state (keyboard_event);
-        xkb_state_update_key (xkb_state, key, state);
+        int state = libinput_event_keyboard_get_key_state(keyboard_event);
+        xkb_state_update_key(xkb_state, key, state);
 
         if (state == LIBINPUT_KEY_STATE_PRESSED)
         {
@@ -36,7 +36,7 @@ process_event(struct libinput_event *event)
                     printf("Key %c was pressed\n", (char)utf8);
                 }
                 else {
-                    printf("The key U+%04X was pressed\n", utf8);
+                    printf("Key U+%04X was pressed\n", utf8);
                 }
             }
         }
@@ -67,33 +67,33 @@ int main()
     struct udev *udev = udev_new();
     struct libinput *libinput = libinput_udev_create_context(&iface, NULL, udev);
 
-	if (!udev) EXIT("Udev error\n");
-	if (!libinput) EXIT("Libinput error\n");
+    if (!udev) EXIT("Udev error\n");
+    if (!libinput) EXIT("Libinput error\n");
 
-	void* libinput_udev_assign(void*, const char*);
-	if (libinput_udev_assign_seat(libinput, "seat0") == -1)
-	{
-		EXIT("cannot assign seat\n");
-	}
+    void* libinput_udev_assign(void*, const char*);
+    if (libinput_udev_assign_seat(libinput, "seat0") == -1)
+    {
+        EXIT("Cannot assign seat\n");
+    }
 
-	xkb_context = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
+    xkb_context = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
 
-	keymap = xkb_keymap_new_from_names(xkb_context, NULL, XKB_KEYMAP_COMPILE_NO_FLAGS);
-	if (!keymap) EXIT("Keymap error\n");
+    xkb_keymap = xkb_keymap_new_from_names(xkb_context, NULL, XKB_KEYMAP_COMPILE_NO_FLAGS);
+    if (!xkb_keymap) EXIT("Keymap error\n");
 
-	xkb_state = xkb_state_new(keymap);
+    xkb_state = xkb_state_new(xkb_keymap);
 
-	while (1) {
-    	struct pollfd fd = {
-        	libinput_get_fd(libinput),
-        	POLLIN,	0
-    	};
+    while (1) {
+        struct pollfd fd = {
+            libinput_get_fd(libinput),
+            POLLIN,	0
+        };
 
-    	poll(&fd, 1, -1);
-    	libinput_dispatch(libinput);
+        poll(&fd, 1, -1);
+        libinput_dispatch(libinput);
 
-    	struct libinput_event *event;
-    	while ((event = libinput_get_event(libinput)))
-        	process_event(event);
-	}
+        struct libinput_event *event;
+        while ((event = libinput_get_event(libinput)))
+            process_event(event);
+    }
 }
